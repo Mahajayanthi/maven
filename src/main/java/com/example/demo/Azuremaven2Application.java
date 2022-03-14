bdea31e1e74a969235b3f8db6cd2ceb6779953a7
Maha Lakshmi <mahamothilal13@gmail.com>
2022-03-14 19:10:10 +0530

    save

---
+++ src/main/java/com/example/demo/Azuremaven2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Azuremaven2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Azuremaven2Application.class, args);
+	}
+
+}
