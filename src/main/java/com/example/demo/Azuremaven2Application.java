c055a0e5ef960f1938f2d3594863491e2f1ce1bb
Mahajayanthi <94959966+Mahajayanthi@users.noreply.github.com>
2022-10-17 17:37:24 +0530

    Update Azuremaven2Application.java

--- src/main/java/com/example/demo/Azuremaven2Application.java
+++ src/main/java/com/example/demo/Azuremaven2Application.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Azuremaven2Application {
+	@RequestMapping("/save)
+	public String hello()
+			{
+				return"welcome to Azure";
+			}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Azuremaven2Application.class, args);
